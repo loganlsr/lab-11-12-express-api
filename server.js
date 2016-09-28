eb66924cee13e98c73779b3f2a2aad7a8d63d87d
Logan Rogers <logan.s.rogers@biola.edu>
2016-09-28 09:45:04 -0700

    error-middleware not working

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ const debug = require('debug')('note:server');
 
 // const cors = require('./lib/cors-middleware.js');
 const appleRouter = require('./route/apple-router.js');
-// const errorMiddleware = require('./lib/error-middleware.js');
+const errorMiddleware = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('dev'));
+// app.use(cors);
 
 app.use(appleRouter);
-
-// app.use(errorMiddleware);
-// app.use(cors);
+app.use(errorMiddleware);
 
 app.listen(PORT, function(){
   debug(`server up ${PORT}`);
